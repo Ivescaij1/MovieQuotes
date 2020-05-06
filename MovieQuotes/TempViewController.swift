f9d38bf3c1714c72cc86a1ef308ef55a503b1d17
ivescai <caij1@rose-hulman.edu>
2020-05-06 01:46:32 -0400

    clean out

---
+++ MovieQuotes/TempViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TempViewController.swift
+//  MovieQuotes
+//
+//  Created by Junfei Cai on 5/6/20.
+//  Copyright © 2020 Junfei Cai. All rights reserved.
+//
+
+import UIKit
+
+class TempViewController: UIViewController {
+    
+}
